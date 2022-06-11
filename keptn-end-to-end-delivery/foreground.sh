7e41cac5703e71553c8e4e050f16cae33942a22b
Adam Gardner <adam@agardner.net>
2022-06-11 18:28:55 +1000

    Update foreground.sh

--- keptn-end-to-end-delivery/foreground.sh
+++ keptn-end-to-end-delivery/foreground.sh
@@ -9,8 +9,7 @@ JOB_EXECUTOR_SERVICE_VERSION=0.2.0
 # ----------------------------------------#
 #         Step 1/5: Installing Helm       #
 # ----------------------------------------#
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && chmod 700 get_helm.sh
 ./get_helm.sh
 
 # -----------------------------------------#
