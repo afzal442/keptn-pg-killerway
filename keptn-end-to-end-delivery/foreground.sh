f55dfb596210322aa155af3243ed07e8279e7494
Adam Gardner <adam@agardner.net>
2022-06-11 18:07:28 +1000

    Update foreground.sh

--- keptn-end-to-end-delivery/foreground.sh
+++ keptn-end-to-end-delivery/foreground.sh
@@ -1,26 +1,28 @@
-# Setting Global variables
+# ----------------------------------------#
+# Setting Global variables                #
+# ----------------------------------------#
 K3D_VERSION=v5.3.0
 KUBECTL_VERSION=v1.22.6
 KEPTN_VERSION=0.16.0
 JOB_EXECUTOR_SERVICE_VERSION=0.2.0
 
 
-# -----
+# ----------------------------------------#
 # Step 1/6: Initialising Kubernetes...
-# -----
+# ----------------------------------------#
 curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=$K3D_VERSION bash
 k3d cluster create mykeptn -p "8080:80@loadbalancer" --k3s-arg "--no-deploy=traefik@server:*"
 
 
-# -----
-# Step 2/6: Installing Kubectl
-# -----
+# ----------------------------------------#
+# Step 2/6: Installing Kubectl            #
+# ----------------------------------------#
 curl -LO https://dl.k8s.io/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-# -----
+# --------------------
 # Step 3/6: Installing Helm
-# -----
+# --------------------
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
