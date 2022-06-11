eb0d565b7370f70ffbf5c6f18d3fbd69d1eb4202
Adam Gardner <adam@agardner.net>
2022-06-12 08:16:19 +1000

    Update foreground.sh

--- keptn-end-to-end-delivery/foreground.sh
+++ keptn-end-to-end-delivery/foreground.sh
@@ -17,7 +17,8 @@ git clone https://github.com/christian-kreuzberger-dtx/keptn-job-executor-delive
 # -----------------------------------------#
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-apt update && apt install gh
+apt update
+apt install gh -y
 
 # -----------------------------------------#
 #      Step 3/9: Installing Keptn CLI      #
