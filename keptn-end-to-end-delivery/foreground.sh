690312f08f97a36c9b46c7217f2f05843fe75217
Adam Gardner <adam@agardner.net>
2022-06-13 09:00:39 +1000

    Update foreground.sh

--- keptn-end-to-end-delivery/foreground.sh
+++ keptn-end-to-end-delivery/foreground.sh
@@ -33,46 +33,51 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 ./get_helm.sh
 
 # ----------------------------------------#
-#      Step 5/11: Installing Kubectl      #
+#      Step 5/12: Installing Kubectl      #
 # ----------------------------------------#
 curl -LO https://dl.k8s.io/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # -----------------------------------------#
-#    Step 6/11: Initialising Kubernetes    #
+#    Step 6/12: Initialising Kubernetes    #
 # -----------------------------------------#
 curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=$K3D_VERSION bash
 k3d cluster create mykeptn -p "8080:80@loadbalancer" --k3s-arg "--no-deploy=traefik@server:*"
 
 
 # -----------------------------------------#
-#    Step 7/11: Installing Prometheus      #
+#    Step 7/12: Installing Prometheus      #
 # -----------------------------------------#
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm install prometheus prometheus-community/prometheus --namespace monitoring --create-namespace
 
 # -------------------------------------------#
-# Step 8/11: Installing Keptn Control Plane  #
+# Step 8/12: Installing Keptn Control Plane  #
 # -------------------------------------------#
 helm install keptn https://github.com/keptn/keptn/releases/download/$KEPTN_VERSION/keptn-$KEPTN_VERSION.tgz -n keptn --timeout=5m --wait --create-namespace --set=control-plane.apiGatewayNginx.type=LoadBalancer
 
 # --------------------------------------------#
-# Step 9/11: Installing Job Executor Service  #
+# Step 9/12: Installing Job Executor Service  #
 # --------------------------------------------#
 KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 -d)
 helm install --namespace keptn-jes --create-namespace --wait --timeout=4m --set=remoteControlPlane.api.hostname=api-gateway-nginx.keptn --set=remoteControlPlane.api.token=$KEPTN_API_TOKEN --set=remoteControlPlane.topicSubscription="sh.keptn.event.je-deployment.triggered\,sh.keptn.event.je-test.triggered\,sh.keptn.event.action.triggered" \
 job-executor-service https://github.com/keptn-contrib/job-executor-service/releases/download/$JOB_EXECUTOR_SERVICE_VERSION/job-executor-service-$JOB_EXECUTOR_SERVICE_VERSION.tgz
 
 # --------------------------------------------#
-# Step 10/11: Installing Prometheus Service   #
+# Step 10/12: Installing Prometheus Service   #
 # --------------------------------------------#
 helm install -n keptn prometheus-service https://github.com/keptn-contrib/prometheus-service/releases/download/$KEPTN_PROMETHEUS_SERVICE_VERSION/prometheus-service-$KEPTN_PROMETHEUS_SERVICE_VERSION.tgz --set resources.requests.cpu=25m
 kubectl -n monitoring apply -f https://raw.githubusercontent.com/keptn-contrib/prometheus-service/$KEPTN_PROMETHEUS_SERVICE_VERSION/deploy/role.yaml
 
 # ---------------------------------------------#
-# Step 11/11: Apply Cluster Admin Role for JES #
+# Step 11/12: Apply Cluster Admin Role for JES #
 # ---------------------------------------------#
 kubectl apply -f ~/keptn-job-executor-delivery-poc/job-executor/workloadClusterRoles.yaml
+
+# ---------------------------------------------#
+# Step 12/12: Authenticate Keptn CLI           #
+# ---------------------------------------------#
+keptn auth --endpoint={{TRAFFIC_HOST1_8080}}/api --api-token=$(kubectl get secret keptn-api-token -n keptn -ojsonpath='{.data.keptn-api-token}' | base64 -d)
 
 # ---------------------------------------------#
 #       🎉 Installation Complete 🎉          #
