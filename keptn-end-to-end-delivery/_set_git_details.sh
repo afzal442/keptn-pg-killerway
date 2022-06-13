85dae5400294b880260fbb126252fbb6a952e325
Adam Gardner <adam@agardner.net>
2022-06-13 11:22:26 +1000

    Update _set_git_details.sh

--- keptn-end-to-end-delivery/_set_git_details.sh
+++ keptn-end-to-end-delivery/_set_git_details.sh
@@ -3,25 +3,4 @@
 # So for now I'll wget it from a private Git in the intro_foreground.sh
 # But this is anticipated to be a temporary workaround and as soon as Killercoda works we'll pull everything from this repo
 #
-echo "#=================================#"
-echo "# Please enter Git details now    #"
-echo "#=================================#"
-read -p 'Git Username: ' GIT_USER
-read -p 'Git Token: ' GITHUB_TOKEN
-read -p 'Git Repo to be Created (eg. keptndemo): ' GIT_NEW_REPO_NAME
-GIT_REPO=https://github.com/$GIT_USER/$GIT_NEW_REPO_NAME.git
-
-echo ""
-echo "#=================================#"
-echo "         Git Details Set:          "
-echo "#=================================#"
-echo ""
-echo "Git Username: $GIT_USER"
-echo "Git Token: $GITHUB_TOKEN"
-echo "New Git repo to be created: $GIT_REPO"
-
-echo ""
-echo "============================================================="
-echo "Made a mistake? Easy. Just click the command again on the left to reset everything."
-echo "Everything look good? Proceed with the tutorial..."
-echo "============================================================="
+# Latest copy on gist.github.com (see intro_foreground.sh)
