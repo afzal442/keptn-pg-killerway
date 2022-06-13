df4eca1fe433abcfd7a47c27980c4be911e2e3ec
Adam Gardner <adam@agardner.net>
2022-06-13 10:21:36 +1000

    Update and rename git_details_foreground.sh to set_git_details.sh

--- keptn-end-to-end-delivery/set_git_details.sh
+++ keptn-end-to-end-delivery/set_git_details.sh
@@ -4,7 +4,7 @@
 read -p 'Git Username: ' GIT_USER
 read -p 'Git Token: ' GITHUB_TOKEN
 read -p 'Git Repo to be Created (eg. keptndemo): ' GIT_NEW_REPO_NAME
-export GIT_REPO=https://github.com/$GIT_USER/$GIT_NEW_REPO_NAME.git
+GIT_REPO=https://github.com/$GIT_USER/$GIT_NEW_REPO_NAME.git
 
 echo ""
 echo "Thanks! Git Details are set. Please proceed."
