59f75f94f8107a238624b7c428a0f840a91e279e
Adam Gardner <adam@agardner.net>
2022-06-14 08:47:08 +1000

    Create quality_gated_release.sh

---
+++ keptn-end-to-end-delivery/assets/scripts/quality_gated_release.sh
@@ -0,0 +1,56 @@
+cd ~/keptn-job-executor-delivery-poc
+echo ""
+echo "===================================================="
+echo "Configuring Keptn to Use Prometheus                 "
+echo "===================================================="
+
+keptn add-resource --project=fulltour --service=helloservice --stage=qa --resource=prometheus/sli.yaml --resourceUri=prometheus/sli.yaml
+keptn add-resource --project=fulltour --service=helloservice --stage=qa --resource=slo.yaml --resourceUri=slo.yaml
+keptn configure monitoring prometheus --project=fulltour --service=helloservice
+
+cd ~/$GIT_NEW_REPO_NAME
+cat << EOF > ~/$GIT_NEW_REPO_NAME/shipyard.yaml
+apiVersion: "spec.keptn.sh/0.2.2"
+kind: "Shipyard"
+metadata:
+  name: "shipyard-delivery"
+spec:
+  stages:
+    - name: "qa"
+      sequences:
+        - name: "delivery"
+          tasks:
+            - name: "je-deployment"
+            - name: "je-test"
+            - name: "evaluation"
+              properties:
+                timeframe: "2m"
+
+    - name: "production"
+      sequences:
+        - name: "delivery"
+          triggeredOn:
+            - event: "qa.delivery.finished"
+          tasks:
+            - name: "approval"
+              properties:
+                pass: "automatic"
+                warning: "automatic"
+            - name: "je-deployment"
+EOF
+git remote set-url origin https://$GIT_USER:$GITHUB_TOKEN@github.com/$GIT_USER/$GIT_NEW_REPO_NAME.git
+git config --global user.email "keptn@keptn.sh"
+git config --global user.name "Keptn"
+git add -A
+git commit -m "add quality evaluation to qa"
+git push
+
+echo ""
+echo "===================================================="
+echo "Trigger another delivery of helloservice v0.1.1     "
+echo "===================================================="
+keptn trigger delivery \
+--project=fulltour \
+--service=helloservice \
+--image="ghcr.io/podtato-head/podtatoserver:v0.1.1" \
+--labels=image="ghcr.io/podtato-head/podtatoserver",version="v0.1.1"
