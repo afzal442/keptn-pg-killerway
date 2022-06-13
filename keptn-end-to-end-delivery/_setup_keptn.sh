62f3bddb39106548170a7b011b4eda32ccaee214
Adam Gardner <adam@agardner.net>
2022-06-14 08:50:58 +1000

    Delete _setup_keptn.sh

--- keptn-end-to-end-delivery/_setup_keptn.sh
+++
@@ -1,7 +0,0 @@
-#
-# IMPORTANT: Killercoda should be able to add this file to the VM but it isn't currently working
-# So for now I'll wget it from a private Git in the intro_foreground.sh
-# But this is anticipated to be a temporary workaround and as soon as Killercoda works we'll pull everything from this repo
-#
-#
-# Latest copy on gist.github.com (see intro_foreground.sh)
