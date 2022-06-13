bc49ce296528866510fe8cbd5ffda4aa03ed75a7
Adam Gardner <adam@agardner.net>
2022-06-13 11:05:23 +1000

    Update and rename set_git_details.sh to _set_git_details.sh

--- keptn-end-to-end-delivery/_set_git_details.sh
+++ keptn-end-to-end-delivery/_set_git_details.sh
@@ -1,3 +1,8 @@
+#
+# IMPORTANT: Killercoda should be able to add this file to the VM but it isn't currently working
+# So for now I'll wget it from a private Git in the intro_foreground.sh
+# But this is anticipated to be a temporary workaround and as soon as Killercoda works we'll pull everything from this repo
+#
 echo "#=================================#"
 echo "# Please enter Git details now    #"
 echo "#=================================#"
