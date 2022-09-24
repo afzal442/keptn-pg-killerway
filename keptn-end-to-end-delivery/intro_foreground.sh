06d2c580cbec133cbc98e7ac758f5ee50adb42b1
Adam Gardner <adam@agardner.net>
2022-09-24 14:03:49 +1000

    Update intro_foreground.sh

--- keptn-end-to-end-delivery/intro_foreground.sh
+++ keptn-end-to-end-delivery/intro_foreground.sh
@@ -1,14 +1,14 @@
 # -----------------------------------------#
 #        Setting Global variables          #
 # -----------------------------------------#
-DEBUG_VERSION=6
+DEBUG_VERSION=7
 K3D_VERSION=v5.3.0
 KUBECTL_VERSION=v1.22.6
 GH_CLI_VERSION=2.15.0
 KEPTN_VERSION=0.18.2
 JOB_EXECUTOR_SERVICE_VERSION=0.3.0
 JOB_EXECUTOR_NAMESPACE=keptn-jes
-KEPTN_PROMETHEUS_SERVICE_VERSION=0.8.6
+KEPTN_PROMETHEUS_SERVICE_VERSION=0.9.1
 PROMETHEUS_VERSION=15.12.0
 
 # ----------------------------------------#
