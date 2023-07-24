0cee03e717fb016ddb2ba086f7408a692275dd5a
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-07-24 10:51:23 +1000

    bump debug version

--- keptn-end-to-end-delivery/intro_foreground.sh
+++ keptn-end-to-end-delivery/intro_foreground.sh
@@ -1,7 +1,7 @@
 # -----------------------------------------#
 #        Setting Global variables          #
 # -----------------------------------------#
-DEBUG_VERSION=2
+DEBUG_VERSION=3
 KUBECTL_VERSION=v1.22.6
 GH_CLI_VERSION=2.23.0
 KEPTN_VERSION=1.4.0
